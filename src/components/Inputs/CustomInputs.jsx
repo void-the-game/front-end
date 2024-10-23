9d18c7a2e5d4893f36dad77662bbf1646cd6b78b
Gustavo <guhh03odelot@gmail.com>
2024-10-23 20:42:09 -0300

    fix: screen-fixes

---
+++ src/components/Inputs/CustomInputs.jsx
@@ -0,0 +1,34 @@
+import './CustomInputs.scss'
+
+
+
+function CustomInput ({border, width, height,type, id, label, placeholder, className,classNameLabel}) {
+    const InputStyle = {
+        width: width || '15rem',
+        height: height || '3rem',
+        border: border || '1px solid #fff',
+        borderRadius: '20px',
+        backgroundColor: '#B1AAFF',
+        padding: '0 10px',
+        color:'#fff',
+        fontSize: '1rem',
+        fontFamily: 'Bruno Ace'
+    };
+
+    return (
+        <div>
+            {label && <label className={classNameLabel}>{label}</label>}
+            <input
+             className={`${className} custom-input`} 
+             style={InputStyle} 
+             placeholder={placeholder || '' }
+             type={type || 'text'}
+             id={id}
+             
+             />
+        </div>
+    );
+}
+
+
+export default CustomInput;
