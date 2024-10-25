c73fdbca1a94f7f5bb6e766c7957fadeba682ce9
Luiz Fernando <luiz.fpimentel0@gmail.com>
2024-10-25 00:09:36 -0300

    fix: remove depraceted config and add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,10 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
+  eslintPluginPrettierRecommended,
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
@@ -33,7 +35,6 @@ export default [
         'warn',
         { allowConstantExport: true }
       ]
-    },
-    extends: ['eslint:recommended', 'plugin:react/recommended']
+    }
   }
 ]
