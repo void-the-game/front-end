79be3f00c99f39f6fb329f0f587b5e1e8d47474f
Gustavo <guhh03odelot@gmail.com>
2024-11-05 22:17:10 -0300

    fix:registration-page-styles

--- src/components/Inputs/CustomInputs.jsx
+++ src/components/Inputs/CustomInputs.jsx
@@ -2,22 +2,16 @@ import './CustomInputs.scss'
 
 
 
-function CustomInput ({border, width, height,type, id, label, placeholder, className,classNameLabel}) {
+function CustomInput ({border,type, id, label, placeholder, className,classNameLabel}) {
     const InputStyle = {
-        width: width || '15rem',
-        height: height || '2rem',
+       
         border: border || '1px solid #fff',
-        borderRadius: '10px',
-        backgroundColor: '#B1AAFF',
-        padding: '0 10px',
-        color:'#fff',
-        fontSize: '1rem',
-        fontFamily: 'Bruno Ace'
+        
     };
 
     return (
-        <div>
-            {label && <label className={classNameLabel}>{label}</label>}
+        <>
+            
             <input
              className={`${className} custom-input`} 
              style={InputStyle} 
@@ -26,7 +20,7 @@ function CustomInput ({border, width, height,type, id, label, placeholder, class
              id={id}
              
              />
-        </div>
+        </>
     );
 }
 
--- src/pages/cadastro/cadastro.jsx
+++ src/pages/cadastro/cadastro.jsx
@@ -9,19 +9,22 @@ import setaDireita from "../../components/img/setaDireita.svg"
 
 function Cadastro() {
   return (
-    <div>
-      <Bolinhas isColored={false} />
+    <>
+      
       <section className="container-cadastro">
+        <Bolinhas isColored={false} />
         <main className="main">
-          <div className="container-candastro-inputs">
+          
             <div className="div-container-inputs">
-              <h2 className="titulo-cadastro">cadastro</h2>
-              <div>
+              <h2 className="titulo-cadastro">CADASTRO</h2>
+              <form action="">
+                <div>
                 <CustomInput
                   placeholder="Usuario"
                   type="text"
                   id="inputUsuario"
                 />
+                {/* <span>error</span> */}
               </div>
               <div>
                 <CustomInput
@@ -29,6 +32,7 @@ function Cadastro() {
                   type="email"
                   id="inputEmail"
                 />
+                {/* <span>error</span> */}
               </div>
               <div>
                 <CustomInput
@@ -36,6 +40,7 @@ function Cadastro() {
                   type="email"
                   id="inputConfEmail"
                 />
+                {/* <span>error</span> */}
               </div>
               <div>
                 <CustomInput
@@ -43,6 +48,11 @@ function Cadastro() {
                   type="password"
                   id="inputPassword"
                 />
+                {/* <span>error</span>
+                <span>error</span>
+                <span>error</span>
+                <span>error</span>
+                <span>error</span> */}
               </div>
               <div>
                 <CustomInput
@@ -50,11 +60,15 @@ function Cadastro() {
                   type="password"
                   id="inputConfPassword"
                 />
+                {/* <span>error</span> */}
               </div>
+              </form>
+              
             </div>
-          </div>
+          
         </main>
-        <div className="div-btnVoltar">
+        <div className="container-buttons">
+          <div className="div-btnVoltar">
           <div className="div-btnVoltar__blur"></div>
           <Link to="/" style={{ textDecoration: "none" }}>
             <CustomButton
@@ -80,8 +94,10 @@ function Cadastro() {
             />
           </Link>
         </div>
+        </div>
+        
       </section>
-    </div>
+    </>
   )
 }
 
