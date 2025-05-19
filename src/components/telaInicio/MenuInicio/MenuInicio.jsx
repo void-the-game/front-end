c250f0e2564fba621d499263a5ef342b619702eb
Luiz Fernando <luiz.fpimentel0@gmail.com>
2025-05-19 01:57:29 -0300

    feat: add click tracking for 'JOGAR' button in MenuInicio component

--- src/components/telaInicio/MenuInicio/MenuInicio.jsx
+++ src/components/telaInicio/MenuInicio/MenuInicio.jsx
@@ -1,14 +1,28 @@
 import CustomButton from '../../Buttons/CustomButton'
 import styles from './MenuInicio.module.scss'
+import { useContext } from 'react'
+import { AbTestContext } from '../../../contexts/AbTestContext'
 
 function MenuInicio() {
+  const variant = useContext(AbTestContext)?.variant
+
+  const handleClick = () => {
+    if (variant) {
+      window.gtag &&
+        window.gtag('event', 'click', {
+          test_name: 'menu_variant',
+          variant,
+        })
+    }
+  }
+
   return (
     <div className={styles['menu-inicio']}>
       <div className={styles.blurback}></div>
       <div className={styles['container-blur2']}>
         <div className={styles.blurback2}></div>
         <div className={styles['borda-menu-div']}>
-          <CustomButton>JOGAR</CustomButton>
+          <CustomButton onClick={handleClick}>JOGAR</CustomButton>
         </div>
 
         <CustomButton>SALAS</CustomButton>
