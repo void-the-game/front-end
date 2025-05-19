7fcd68a6f5c5e5a5e7f2b6551c81724e9c9397ee
Luiz Fernando <luiz.fpimentel0@gmail.com>
2025-05-19 01:39:48 -0300

    feat: implement A/B testing for menu variant selection in Home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,30 @@
 import Bolinhas from '../../components/bolinhas/Bolinhas'
 import Titulo from '../../components/telaInicio/titulo/Titulo'
 import MenuEntrar from '../../components/telaInicio/MenuEntrar/MenuEntrar'
-import MenuInicio from '../../components/telaInicio/menu-inicio/MenuInicio'
+import MenuInicio from '../../components/telaInicio/MenuInicio/MenuInicio'
+import MenuInicioVariantB from '../../components/telaInicio/MenuInicioVariantB/MenuInicioVariantB'
 import styles from './Home.module.scss'
 import purpleAvatar from '../../components/img/purpleavatar.svg'
 import configIcon from '../../components/img/configurationIcon.svg'
 import CustomButton from '../../components/Buttons/CustomButton'
-import { useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { BsQuestionLg } from 'react-icons/bs'
+import { AbTestContext } from '../../contexts/AbTestContext'
 
 function Home() {
   const [user] = useState(localStorage.getItem('@Void:user') || null)
+
+  const variant = useContext(AbTestContext)?.variant
+
+  useEffect(() => {
+    if (variant) {
+      window.gtag &&
+        window.gtag('event', 'page_view', {
+          test_name: 'menu_variant',
+          variant,
+        })
+    }
+  }, [variant])
 
   return (
     <div className={styles.app}>
@@ -20,7 +34,7 @@ function Home() {
           <Titulo />
         </div>
         <div className={styles['container-menu-inicio']}>
-          <MenuInicio />
+          {variant === 'A' ? <MenuInicio /> : <MenuInicioVariantB />}
         </div>
         {!user ? (
           <div>
