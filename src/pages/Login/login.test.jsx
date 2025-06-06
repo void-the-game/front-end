1dd4b77b9f1ce5f3cef458aa52620174b045c4d3
Guilherme Lopreti <guilherme.llopreti@gmail.com>
2025-06-06 14:07:42 -0300

    test: login and register page with api integration and home page navigation

--- src/pages/Login/login.test.jsx
+++ src/pages/Login/login.test.jsx
@@ -2,17 +2,24 @@ import { fireEvent, render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
 import Login from '.'
+import { apiDev } from '../../services/api'
+import MockAdapter from 'axios-mock-adapter'
+import { ToastContainer } from 'react-toastify'
+
+const apiMock = new MockAdapter(apiDev)
 
 // Mock do useNavigate
+const mockNavigate = vi.fn()
+
 vi.mock('react-router-dom', async () => {
   const actual = await vi.importActual('react-router-dom')
   return {
     ...actual,
-    useNavigate: () => vi.fn(),
+    useNavigate: () => mockNavigate,
   }
 })
 
-describe('Login Page Unitary Testing', () => {
+describe('Login Page Unit Testing', () => {
   test('Should show message erros', async () => {
     render(<Login />)
 
@@ -31,5 +38,73 @@ describe('Login Page Unitary Testing', () => {
 
     //Campo de senha não preenchido
     expect(screen.getByText('Campo Obrigatório')).toBeTruthy()
+  })
+})
+
+describe('Login Page Integration Testing', () => {
+  test('Should fail to sign in', async () => {
+    apiMock.onPost('/user/login').reply(400, {})
+
+    // Silencia console.log temporariamente
+    const consoleLogMock = vi.spyOn(console, 'log').mockImplementation(() => {})
+
+    render(
+      <>
+        <Login />
+        <ToastContainer />
+      </>,
+    )
+
+    const emailField = screen.getByPlaceholderText('E-mail')
+    const passwordField = screen.getByPlaceholderText('Senha')
+
+    await userEvent.type(emailField, 'teste@mail.com')
+    await userEvent.type(passwordField, 'Void@@68465')
+
+    const button = screen.getByText('Entrar')
+    await userEvent.click(button)
+
+    expect(
+      screen.getByText('Erro na autenticação, verifique seu e-mail ou senha'),
+    ).toBeTruthy()
+    expect(
+      screen.getByText('Erro na autenticação, verifique seu e-mail ou senha'),
+    ).toBeInTheDocument()
+
+    expect(mockNavigate).not.toHaveBeenCalled()
+
+    // Restaura console.log
+    consoleLogMock.mockRestore()
+  })
+
+  test('Should be able to sign in', async () => {
+    apiMock.onPost('/user/login').reply(200, {
+      accessToken: 'fake-token',
+      username: 'teste',
+    })
+
+    render(
+      <>
+        <Login />
+        <ToastContainer />
+      </>,
+    )
+
+    const emailField = screen.getByPlaceholderText('E-mail')
+    const passwordField = screen.getByPlaceholderText('Senha')
+
+    await userEvent.type(emailField, 'teste@mail.com')
+    await userEvent.type(passwordField, 'Voidtest$1234')
+
+    const button = screen.getByText('Entrar')
+    await userEvent.click(button)
+
+    expect(screen.getByText('Bem-Vindo ao VOID!')).toBeTruthy()
+    expect(screen.getByText('Bem-Vindo ao VOID!')).toBeInTheDocument()
+
+    expect(mockNavigate).toHaveBeenCalledWith('/')
+
+    expect(localStorage.getItem('@Void:token')).toBe('fake-token')
+    expect(localStorage.getItem('@Void:user')).toBe('teste')
   })
 })
---
+++ src/pages/cadastro/cadastro.test.jsx
@@ -0,0 +1,94 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+import Cadastro from './cadastro'
+import { apiDev } from '../../services/api'
+import MockAdapter from 'axios-mock-adapter'
+import { ToastContainer } from 'react-toastify'
+
+const apiMock = new MockAdapter(apiDev)
+
+// Mock do useNavigate
+const mockNavigate = vi.fn()
+
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom')
+  return {
+    ...actual,
+    useNavigate: () => mockNavigate,
+  }
+})
+
+describe('Register Page Integration Testing', () => {
+  test('Should fail to register a new user', async () => {
+    apiMock.onPost('/user/create').reply(400, {})
+
+    // Silencia console.log temporariamente
+    const consoleLogMock = vi.spyOn(console, 'log').mockImplementation(() => {})
+
+    render(
+      <>
+        <Cadastro />
+        <ToastContainer />
+      </>,
+    )
+
+    const userField = screen.getByPlaceholderText('Usuario')
+    const emailField = screen.getByPlaceholderText('E-mail')
+    const emailConfirmField = screen.getByPlaceholderText('Confirme seu E-mail')
+    const passwordField = screen.getByPlaceholderText('Senha')
+    const passwordConfirmField =
+      screen.getByPlaceholderText('Confirme sua Senha')
+
+    await userEvent.type(userField, 'teste da silva')
+    await userEvent.type(emailField, 'teste@mail.com')
+    await userEvent.type(emailConfirmField, 'teste@mail.com')
+    await userEvent.type(passwordField, 'Void@@68465')
+    await userEvent.type(passwordConfirmField, 'Void@@68465')
+
+    const button = screen.getByText('Seguir')
+    await userEvent.click(button)
+
+    expect(screen.getByText('Erro no cadastro')).toBeTruthy()
+    expect(screen.getByText('Erro no cadastro')).toBeInTheDocument()
+
+    expect(mockNavigate).not.toHaveBeenCalled()
+
+    // Restaura console.log
+    consoleLogMock.mockRestore()
+  })
+
+  test('Should be able to register a new user', async () => {
+    apiMock.onPost('/user/create').reply(201, {})
+
+    render(
+      <>
+        <Cadastro />
+        <ToastContainer />
+      </>,
+    )
+
+    const userField = screen.getByPlaceholderText('Usuario')
+    const emailField = screen.getByPlaceholderText('E-mail')
+    const emailConfirmField = screen.getByPlaceholderText('Confirme seu E-mail')
+    const passwordField = screen.getByPlaceholderText('Senha')
+    const passwordConfirmField =
+      screen.getByPlaceholderText('Confirme sua Senha')
+
+    await userEvent.type(userField, 'teste da silva')
+    await userEvent.type(emailField, 'teste@mail.com')
+    await userEvent.type(emailConfirmField, 'teste@mail.com')
+    await userEvent.type(passwordField, 'Void@@68465')
+    await userEvent.type(passwordConfirmField, 'Void@@68465')
+
+    const button = screen.getByText('Seguir')
+    await userEvent.click(button)
+
+    expect(screen.getByText('Cadastro realizado com sucesso!')).toBeTruthy()
+    expect(
+      screen.getByText('Cadastro realizado com sucesso!'),
+    ).toBeInTheDocument()
+
+    expect(mockNavigate).toHaveBeenCalledWith('/login')
+  })
+})
