54e7142caae0ff44d3a5e37fee07e61d72c73ed9
Guilherme Lopreti <guilherme.llopreti@gmail.com>
2025-06-11 01:54:18 -0300

    feat: add configs to generate source maps

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,13 @@ Sentry.init({
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
-  integrations: [Sentry.replayIntegration()],
+  integrations: [Sentry.replayIntegration()], //Habilita replays de ações no sentry
+
   // Session Replay
-  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+
+  // gera replay de 10% das seções gerais
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  // gera replay de 100% das seções que geram erros
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 })
 
